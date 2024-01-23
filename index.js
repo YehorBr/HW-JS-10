4ae2c92706907af39cd306d2b40973a7995e3a3a
Yehor <yehor12344321@gmail.com>
2024-01-23 18:47:51 +0200

    зберігає зміни

---
+++ index.js
@@ -0,0 +1,30 @@
+// const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
+
+// let string = '';
+
+// for(let friend of friends){
+//     let friendstr = friend + ", "
+//     string += friendstr
+// }
+
+// console.log(string);
+
+// string = friends.join(', ');
+
+// console.log(string);
+
+// const cards = ['card1', 'card2', 'card3', 'card4', 'card5']
+
+// const cardToRemove = cards.splice(2, 1);
+
+// console.log(cardToRemove);
+
+// const cardToInsert = cards.splice(5, 0, 'card6');
+
+// console.log(cards);
+
+// const cardToUpdate = cards.splice(3, 1, 'card4-new');
+
+// console.log(cards);
+
+
